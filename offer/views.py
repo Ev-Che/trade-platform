1c81671356676abe8096c23708ec8255c6bab50b
Ev-Che <ev.che2001@gmail.com>
2021-11-18 16:10:12 +0300

    Start offer app

--- offer/views.py
+++ offer/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Offer
+from .serializers import OfferSerializer
+
+
+class OfferViewSet(viewsets.ModelViewSet):
+    serializer_class = OfferSerializer
+    queryset = Offer.objects.all()
+    permission_classes = [IsAuthenticated]
