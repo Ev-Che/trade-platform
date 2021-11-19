aa18e7a20963b973b52721b24e50bd677f7a7a64
Ev-Che <ev.che2001@gmail.com>
2021-11-19 10:45:51 +0300

    Add all CRUD to stock management

--- stock_management/views.py
+++ stock_management/views.py
@@ -1,10 +1,19 @@
 from rest_framework import viewsets
 
-from .models import Stock
-from .serializers import StockSerializer
+from .models import Stock, Price, Currency
+from .serializers import StockSerializer, PriceSerializer, CurrencySerializer
 
 
-class StockViewSet(viewsets.ReadOnlyModelViewSet):
-    """View for listing and retrieving stocks"""
+class StockViewSet(viewsets.ModelViewSet):
     serializer_class = StockSerializer
     queryset = Stock.objects.all()
+
+
+class PriceViewSet(viewsets.ModelViewSet):
+    serializer_class = PriceSerializer
+    queryset = Price.objects.all()
+
+
+class CurrencyViewSet(viewsets.ModelViewSet):
+    serializer_class = CurrencySerializer
+    queryset = Currency.objects.all()
